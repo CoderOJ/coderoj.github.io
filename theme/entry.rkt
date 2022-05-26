5d47e8fdabf59429b4841d80e4b4de39066e1b4c
CoderOJ <CoderOJ@outlook.com>
2022-05-26 09:59:01 +0800

    feat(theme/entry): add option to parse single file

--- theme/entry.rkt
+++ theme/entry.rkt
@@ -5,49 +5,44 @@
 (require "../article.rkt")
 (require "components.rkt")
 
-(define (art-changed name)
-  (define src-dir (format "source/articles/~a/" name))
-  (define dest-dir (format "public/articles/~a/" name))
-  (if (directory-exists? dest-dir)
-    (> (file-or-directory-modify-seconds src-dir)
-       (file-or-directory-modify-seconds dest-dir))
-    #t))
+(define conf (dict ($open "source/config.ss")))
 
 (define (init)
   ($exec "rm -r public")
   ($exec "cp -r theme/static public"))
 
+(define (parse-art title)
+  (printf "parsing ~v\n" title)
+  (cons title ($load-article (format "source/articles/~a/index.ss" title))))
+
+(define (gen-art art)
+  (printf "generate ~v\n" (car art))
+  (define src-dir (format "source/articles/~a/" (car art)))
+  (define dest-dir (format "public/articles/~a/" (car art)))
+  (make-directory* dest-dir)
+  (when (not (assv 'password (cadr art)))
+    ($exec (format "cp -r ~a* ~a" src-dir dest-dir)))
+  ($write (format "~aindex.html" dest-dir) ($html (@/articles/title conf (cdr art)))))
+
+(define (gen-single title)
+  (define art (parse-art title))
+  (gen-art art))
+
 (define (gen)
-  (define conf (dict ($open "source/config.ss")))
-
   (define arts
-    (map
-     (lambda (title)
-       (printf "parsing ~v\n" title)
-       (cons title ($load-article (format "source/articles/~a/index.ss" title))))
-     (filter art-changed
-       (cdr (assv 'articles conf)))))
+    (map parse-art (cdr (assv 'articles conf))))
 
-  ; ($exec "rm -r public")
-  ; ($exec "cp -r theme/static public")
+  (init)
   (make-directory* "public/articles")
   ($write "public/index.html" ($html (@/home conf)))
   ($write "public/articles/index.html" ($html (@/articles conf arts)))
-  (map
-   (lambda (art)
-     (printf "generate ~v\n" (car art))
-     (define src-dir (format "source/articles/~a/" (car art)))
-     (define dest-dir (format "public/articles/~a/" (car art)))
-     (make-directory* dest-dir)
-     (when (not (assv 'password (cadr art)))
-       ($exec (format "cp -r ~a* ~a" src-dir dest-dir)))
-     ($write (format "~aindex.html" dest-dir) ($html (@/articles/title conf (cdr art)))))
-   arts)
+  (map gen-art arts)
   (displayln "finish"))
 
 (define ($entry argv)
   (match argv
     ['#() (gen)]
+    [`#("gen" ,title) (gen-single title)]
     ['#("gen") (gen)]
     ['#("init") (init)]
     [else (printf "error: unkown command ~a\n" argv)]))
